0f7108888d0fee19890b3a04c2fd52cd1b7045b0
Ricardo Gonzalez Rangel <r.grangel@hotmail.com>
2017-12-04 13:10:10 -0600

    Added form component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { ContactFormComponent } from './contact-form/contact-form.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactFormComponent
   ],
   imports: [
     BrowserModule
